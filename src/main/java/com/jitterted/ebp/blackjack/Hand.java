246f1659f2d98deffa7ef0178acd951add5822cb
Ted M. Young <tedyoung@gmail.com>
2021-11-15 12:12:35 -0800

    Lab #2: Made value() public (as a result of inlining displayValue) and removed valueEquals as it's no longer needed.

--- src/main/java/com/jitterted/ebp/blackjack/Hand.java
+++ src/main/java/com/jitterted/ebp/blackjack/Hand.java
@@ -13,7 +13,7 @@ public class Hand {
     public Hand() {
     }
 
-    private int value() {
+    public int value() {
         int handValue = cards
                 .stream()
                 .mapToInt(Card::rankValue)
@@ -61,11 +61,4 @@ public class Hand {
         return hand.value() < value();
     }
 
-    String displayValue() {
-        return String.valueOf(value());
-    }
-
-    public boolean valueEquals(int target) {
-        return value() == target;
-    }
 }
