998e0ac6827e1f28b95cfcc4b9eea44cc3fe9de7
Ted M. Young <tedyoung@gmail.com>
2021-11-15 12:05:37 -0800

    Lab #2: Moved cardsAsString to ConsoleHand. Inlined Hand display into Game.

--- src/main/java/com/jitterted/ebp/blackjack/ConsoleHand.java
+++ src/main/java/com/jitterted/ebp/blackjack/ConsoleHand.java
@@ -1,10 +1,20 @@
 package com.jitterted.ebp.blackjack;
 
+import java.util.stream.Collectors;
+
+import static org.fusesource.jansi.Ansi.ansi;
+
 public class ConsoleHand {
 
     // TRANSFORMER/TRANSLATOR: DOMAIN (Hand) -- first card --> CONSOLE ADAPTER (String)
-    static String displayFirstCard(Hand hand) {
+    static String displayFaceUpCard(Hand hand) {
         return ConsoleCard.display(hand.faceUpCard());
     }
 
+    public static String cardsAsString(Hand hand) {
+        return hand.cards().stream()
+                   .map(ConsoleCard::display)
+                   .collect(Collectors.joining(
+                           ansi().cursorUp(6).cursorRight(1).toString()));
+    }
 }
